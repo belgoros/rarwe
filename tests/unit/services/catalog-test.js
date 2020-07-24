e49769cdc056aaf77c2c0c02c9555e7a34221ea3
Serguei Cambour <s.cambour@gmail.com>
2020-07-24 12:41:44 +0200

    Rename service catalogue -> catalog

--- tests/unit/services/catalog-test.js
+++ tests/unit/services/catalog-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Service | catalogue', function(hooks) {
+module('Unit | Service | catalog', function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let service = this.owner.lookup('service:catalogue');
+  test('it exists', function (assert) {
+    let service = this.owner.lookup('service:catalog');
     assert.ok(service);
   });
 });
