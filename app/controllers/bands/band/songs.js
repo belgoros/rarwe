8b2ad561151fc9c8c84c6b85a591856db4d07b9c
Serguei Cambour <s.cambour@gmail.com>
2020-07-24 14:41:54 +0200

    Extract band and song into separate models

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,12 +1,11 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { Song } from 'rarwe/routes/bands';
+import Song from 'rarwe/models/song';
 import { inject as service } from '@ember/service';
 export default class BandsBandSongsController extends Controller {
   @tracked showAddSong = true;
   @tracked title = '';
-
   @service catalog;
 
   @action
