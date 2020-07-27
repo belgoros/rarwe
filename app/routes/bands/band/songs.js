3bb46672953af51143fb280d701490c61a0eadf0
Serguei Cambour <s.cambour@gmail.com>
2020-07-27 12:56:02 +0200

    Enrich Catalog service to load and update songs and bands

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -7,23 +7,8 @@ export default class BandsBandSongsRoute extends Route {
 
   async model() {
     let band = this.modelFor('bands.band');
-    let url = band.relationships.songs;
-    let response = await fetch(url);
-    let json = await response.json();
-    let songs = [];
+    await this.catalog.fetchRelated(band, 'songs');
 
-    for (let item of json.data) {
-      let { id, attributes, relationships } = item;
-      let rels = {};
-
-      for (let relationshipName in relationships) {
-        rels[relationshipName] = relationships[relationshipName].links.related;
-      }
-      let song = new Song({ id, ...attributes }, rels);
-      songs.push(song);
-      this.catalog.add('song', song);
-    }
-    band.songs = songs;
     return band;
   }
 
