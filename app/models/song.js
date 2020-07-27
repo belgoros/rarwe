b92e4320ac64d23756bdb150c25b384513d22609
Serguei Cambour <s.cambour@gmail.com>
2020-07-27 12:19:41 +0200

    Refactor updateRating function

--- app/models/song.js
+++ app/models/song.js
@@ -1,6 +1,8 @@
+import { tracked } from '@glimmer/tracking';
+
 export default class Song {
   title = '';
-  rating = 0;
+  @tracked rating = 0;
   band = null;
 
   constructor({ title, rating, band }, relationships = {}) {
