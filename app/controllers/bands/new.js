3cb0bda1583c614aa9e330dcc95c28211ad99c3b
Serguei Cambour <s.cambour@gmail.com>
2020-07-28 12:21:00 +0200

    Add routeWillChange to bands.new controller

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -6,9 +6,32 @@ export default class BandsNewController extends Controller {
   @service catalog;
   @service router;
 
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', transition => {
+      if (transition.isAborted) {
+        return;
+      }
+      if (this.confirmedLeave) {
+        return;
+      }
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (leave) {
+            this.confirmedLeave = true;
+          } else {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
   async saveBand() {
     let band = await this.catalog.create('band', { name: this.name });
+    this.confirmedLeave = true;
     this.router.transitionTo('bands.band.songs', band.id);
   }
 }
